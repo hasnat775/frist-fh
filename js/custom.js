c01d241b663a6992ecb15c37079514d4e9342775
Md. Abul Hasnat <74089806+hasnat775@users.noreply.github.com>
2020-11-07 13:51:26 +0600

    upload all files

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+ 
+	 // img-popup activation
+	 $('.btn').magnificPopup({
+	   type: 'image'
+	});
+
+
+	 // video2 activation
+	 $('.btn2').magnificPopup({
+	   type: 'iframe',
+	});
+
+	 // video2 activation
+	 $('.btn3').magnificPopup({
+	   type: 'iframe',
+	});
+});
